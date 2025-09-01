1b56c730d9462c39e4c0f03735a0d3c9409e455f
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-09-01 22:45:51 +0300

    feat(models): добавил ORM-модели Question и Answer

---
+++ app/models.py
@@ -0,0 +1,59 @@
+"""
+ORM-модели: Вопрос и ответ.
+"""
+
+from datetime import datetime
+
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    """Базовый класс для всех моделей."""
+
+    pass
+
+
+class Question(Base):
+    """Модель Вопроса."""
+
+    __tablename__ = "question"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    # Один вопрос может иметь несколько ответов.
+
+    answers: Mapped[list["Answer"]] = relationship(
+        back_populates="question",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+        order_by="Answer.id",
+    )
+
+
+class Answer(Base):
+    """Модель ответа."""
+
+    __tablename__ = "answers"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    question_id: Mapped[str] = mapped_column(
+        ForeignKey("question.id", ondelete="CASCADE"),
+        index=True,
+    )
+    user_id: Mapped[str] = (mapped_column(String(36), index=True),)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    # каждый ответ принадлежит одному вопросу.
+    question: Mapped["Question"] = relationship(back_populates="answers")
